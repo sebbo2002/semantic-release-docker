c266619159154233be9b24412745bb134564962f
Sebastian Pekarek <mail@sebbo.net>
2021-07-26 21:21:10 +0200

    feat: first commit

---
+++ src/lib/types.ts
@@ -0,0 +1,35 @@
+export interface PluginConfig {
+    images: string | string[];
+    tag?: {
+        latest?: boolean;
+        major?: boolean;
+        minor?: boolean;
+        version?: boolean;
+        channel?: boolean;
+    }
+}
+
+export interface NormalizedPluginConfigTags {
+    latest: boolean;
+    major: boolean;
+    minor: boolean;
+    version: boolean;
+    channel: boolean;
+}
+
+export type PluginConfigTagKeys = keyof NormalizedPluginConfigTags;
+
+export interface NormalizedPluginConfig {
+    images: string[];
+    tag: NormalizedPluginConfigTags
+}
+
+export interface MajorAndMinorPart {
+    major: string | null;
+    minor: string | null;
+}
+
+export interface TagTask {
+    input: string;
+    output: string;
+}
--- test/lib/index.ts
+++ test/lib/index.ts
@@ -1,10 +1,307 @@
 'use strict';
 
-import Magic from '../../src/lib';
-import assert = require('assert');
+import {getBaseImage, getMajorAndMinorPart, getTagTasks, isNoPreRelease, parseConfig, tagImage} from '../../src/lib';
+import assert from 'assert';
 
-describe('Example', function () {
-    it('should work with integers', function () {
-        assert.strictEqual(Magic.double(2), 4);
+describe('@sebbo2002/semantic-release-docker', function () {
+    describe('parseConfig', function () {
+        it('should work without any tags', function () {
+            const result = parseConfig({
+                images: ['ubuntu']
+            });
+            assert.deepStrictEqual(result, {
+                images: ['ubuntu'],
+                tag: {
+                    latest: true,
+                    major: true,
+                    minor: true,
+                    version: true,
+                    channel: true
+                }
+            });
+        });
+        it('should work with images string', function () {
+            const result = parseConfig({
+                images: 'ubuntu'
+            });
+            assert.deepStrictEqual(result, {
+                images: ['ubuntu'],
+                tag: {
+                    latest: true,
+                    major: true,
+                    minor: true,
+                    version: true,
+                    channel: true
+                }
+            });
+        });
+        it('should merge tags', function () {
+            const result = parseConfig({
+                images: ['ubuntu'],
+                tag: {
+                    minor: false
+                }
+            });
+            assert.deepStrictEqual(result, {
+                images: ['ubuntu'],
+                tag: {
+                    latest: true,
+                    major: true,
+                    minor: false,
+                    version: true,
+                    channel: true
+                }
+            });
+        });
+        it('should throw error without image defined', function () {
+            assert.throws(() => {
+                // @ts-ignore
+                parseConfig({});
+            }, /Configuration invalid: No image defined!/);
+        });
+    });
+    describe('getBaseImage', function() {
+        it('alpine', function () {
+            assert.strictEqual(getBaseImage('alpine'), 'alpine');
+        });
+        it('ubuntu:14', function () {
+            assert.strictEqual(getBaseImage('ubuntu:14'), 'ubuntu');
+        });
+        it('sebbo2002/test:14.04', function () {
+            assert.strictEqual(getBaseImage('sebbo2002/test:14.04'), 'sebbo2002/test');
+        });
+        it('ubuntu@14', function () {
+            assert.strictEqual(getBaseImage('ubuntu@14'), 'ubuntu');
+        });
+        it('sebbo2002/test@14.04', function () {
+            assert.strictEqual(getBaseImage('sebbo2002/test@14.04'), 'sebbo2002/test');
+        });
+        it('alpine@sha256:aaaaf56b44807c64d294e6c8059b479f35350b454492398225034174808d1726', function () {
+            assert.strictEqual(getBaseImage(
+                'alpine@sha256:aaaaf56b44807c64d294e6c8059b479f35350b454492398225034174808d1726'
+            ), 'alpine');
+        });
+        it('quay.io/signalfuse/zookeeper', function () {
+            assert.strictEqual(getBaseImage('quay.io/signalfuse/zookeeper'), 'quay.io/signalfuse/zookeeper');
+        });
+        it('internal.mycorp.com:5000/revealjs', function () {
+            assert.strictEqual(
+                getBaseImage('internal.mycorp.com:5000/revealjs'),
+                'internal.mycorp.com:5000/revealjs'
+            );
+        });
+        it('internal.mycorp.com:5000/revealjs:3.4.5-3-develop.5', function () {
+            assert.strictEqual(
+                getBaseImage('internal.mycorp.com:5000/revealjs:3.4.5-3-develop.5'),
+                'internal.mycorp.com:5000/revealjs'
+            );
+        });
+    });
+    describe('isNoPreRelease', function() {
+        it('with release', function () {
+            const result = isNoPreRelease({
+                logger: {
+                    log: () => {},
+                    error: () => {}
+                },
+                env: {},
+                nextRelease: {
+                    type: 'patch',
+                    version: '8.2.6',
+                    notes: '',
+                    gitHead: '',
+                    gitTag: ''
+                }
+            });
+
+            assert.strictEqual(result, true);
+        });
+        it('with pre-release', function () {
+            const result = isNoPreRelease({
+                logger: {
+                    log: () => {},
+                    error: () => {}
+                },
+                env: {},
+                nextRelease: {
+                    // @ts-ignore
+                    type: 'prerelase',
+                    version: '8.2.6-develop.1',
+                    notes: '',
+                    gitHead: '',
+                    gitTag: ''
+                }
+            });
+
+            assert.strictEqual(result, false);
+        });
+        it('without release', function () {
+            const result = isNoPreRelease({
+                logger: {
+                    log: () => {},
+                    error: () => {}
+                },
+                env: {}
+            });
+
+            assert.strictEqual(result, false);
+        });
+    });
+    describe('getMajorAndMinorPart', function () {
+        it('undefined', function() {
+            assert.deepStrictEqual(getMajorAndMinorPart(undefined), {
+                major: null,
+                minor: null
+            });
+        });
+        it('4.6.1', function() {
+            assert.deepStrictEqual(getMajorAndMinorPart('4.6.1'), {
+                major: '4',
+                minor: '6'
+            });
+        });
+        it('4.6.1-develop.12', function() {
+            assert.deepStrictEqual(getMajorAndMinorPart('4.6.1-develop.12'), {
+                major: '4',
+                minor: '6'
+            });
+        });
+    });
+    describe('getTagTasks', function () {
+        it('should quietly abort if no release should be triggered', function () {
+            getTagTasks({
+                images: ['ubuntu'],
+                tag: {
+                    latest: true,
+                    major: true,
+                    minor: true,
+                    version: true,
+                    channel: true
+                }
+            }, {
+                logger: {
+                    log: () => {},
+                    error: () => {}
+                },
+                env: {}
+            });
+        });
+        it('patch release', function () {
+            const result = getTagTasks({
+                images: [
+                    'alpine@sha256:aaaaf56b44807c64d294e6c8059b479f35350b454492398225034174808d1726',
+                    'sebbo2002/test:1234567'
+                ],
+                tag: {
+                    latest: true,
+                    major: true,
+                    minor: true,
+                    version: true,
+                    channel: true
+                }
+            }, {
+                logger: {
+                    log: () => {},
+                    error: () => {}
+                },
+                env: {},
+                nextRelease: {
+                    type: 'patch',
+                    version: '8.2.6',
+                    notes: '',
+                    gitHead: '',
+                    gitTag: ''
+                }
+            });
+
+            assert.deepStrictEqual(result, [
+                {
+                    input: 'alpine@sha256:aaaaf56b44807c64d294e6c8059b479f35350b454492398225034174808d1726',
+                    output: 'alpine:latest'
+                },
+                {
+                    input: 'alpine@sha256:aaaaf56b44807c64d294e6c8059b479f35350b454492398225034174808d1726',
+                    output: 'alpine:8'
+                },
+                {
+                    input: 'alpine@sha256:aaaaf56b44807c64d294e6c8059b479f35350b454492398225034174808d1726',
+                    output: 'alpine:8.2'
+                },
+                {
+                    input: 'alpine@sha256:aaaaf56b44807c64d294e6c8059b479f35350b454492398225034174808d1726',
+                    output: 'alpine:8.2.6'
+                },
+                {
+                    input: 'sebbo2002/test:1234567',
+                    output: 'sebbo2002/test:latest'
+                },
+                {
+                    input: 'sebbo2002/test:1234567',
+                    output: 'sebbo2002/test:8'
+                },
+                {
+                    input: 'sebbo2002/test:1234567',
+                    output: 'sebbo2002/test:8.2'
+                },
+                {
+                    input: 'sebbo2002/test:1234567',
+                    output: 'sebbo2002/test:8.2.6'
+                }
+            ]);
+        });
+        it('pre release', function () {
+            const result = getTagTasks({
+                images: [
+                    'alpine@sha256:aaaaf56b44807c64d294e6c8059b479f35350b454492398225034174808d1726',
+                    'sebbo2002/test:1234567'
+                ],
+                tag: {
+                    latest: true,
+                    major: true,
+                    minor: true,
+                    version: true,
+                    channel: true
+                }
+            }, {
+                logger: {
+                    log: () => {},
+                    error: () => {}
+                },
+                env: {},
+                nextRelease: {
+                    // @ts-ignore
+                    type: 'prerelease',
+                    version: '8.2.6-develop.3',
+                    notes: '',
+                    gitHead: '',
+                    gitTag: '',
+                    channel: 'next'
+                }
+            });
+
+            assert.deepStrictEqual(result, [
+                {
+                    input: 'alpine@sha256:aaaaf56b44807c64d294e6c8059b479f35350b454492398225034174808d1726',
+                    output: 'alpine:8.2.6-develop.3'
+                },
+                {
+                    input: 'alpine@sha256:aaaaf56b44807c64d294e6c8059b479f35350b454492398225034174808d1726',
+                    output: 'alpine:next'
+                },
+                {
+                    input: 'sebbo2002/test:1234567',
+                    output: 'sebbo2002/test:8.2.6-develop.3'
+                },
+                {
+                    input: 'sebbo2002/test:1234567',
+                    output: 'sebbo2002/test:next'
+                }
+            ]);
+        });
+    });
+    describe('tagImage', function() {
+        it('tag ubuntu', async function() {
+            await tagImage('alpine', 'alpine:tagging-test');
+        });
     });
 });
