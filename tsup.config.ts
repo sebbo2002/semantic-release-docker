92bfa4c97dc3196433f4764ab950d32e93c29e10
Sebastian Pekarek <mail@sebbo.net>
2024-09-09 10:48:40 +0200

    fix: Fix tsup config / bundled modules
    There where packages bundled which are not even used… :/

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,9 @@
 import { defineConfig } from 'tsup';
+import fs from 'fs';
+
+const pkg = JSON.parse(
+    fs.readFileSync('./package.json', 'utf-8')
+);
 
 export default defineConfig({
     clean: true,
@@ -7,6 +12,8 @@ export default defineConfig({
         'src/bin/cli.ts',
         'src/bin/start.ts'
     ],
+    external: [...Object.keys(pkg.devDependencies), 'semantic-release'],
+    noExternal: Object.keys(pkg.dependencies),
     format: ['esm'],
     dts: true,
     sourcemap: true,
